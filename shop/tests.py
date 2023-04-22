0eda334eebbe4c7e939e129ab7141feaf9a6c8fe
Dmitry Kovalev <pzhabbiu@gmail.com>
2023-04-22 15:35:52 +0300

    test, views

--- shop/tests.py
+++ shop/tests.py
@@ -1,3 +1,41 @@
+from django.contrib.auth import get_user_model
+# from somelib import send_email_notification
 from django.test import TestCase
 
-# Create your tests here.
+
+User = get_user_model()
+
+
+def user_log_in(email, password):
+    try:
+        user = User.objects.get(email=email)
+    except User.DoesNotExists:
+        raise ValueError('User not found')
+
+    if user.check_password(password):
+        # send_email_notification('Вы вошли в систему')
+        return True
+    else:
+        return False
+
+
+class LoginTest(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.user = User.objects.create_user(username='auth',
+                                            email='e@ma.il',
+                                            password='123')
+
+    def test_get_user_false(self):
+        """Проверяем вызов исключения при поиске пользователя."""
+        self.assertRaisesRegex(ValueError,
+                               'User not found',
+                               user_log_in('False', 'False'))
+
+    def test_get_user_true(self):
+        """Проверяем результат отправки сообщения."""
+        user_false = user_log_in(self.user.email, 'False')
+        user_true = user_log_in(self.user.email, self.user.password)
+        self.assertFalse(user_false)
+        self.assertTrue(user_true)
--- shop/views.py
+++ shop/views.py
@@ -1,12 +1,21 @@
 from django.contrib.auth import get_user_model
 from django.shortcuts import render, get_object_or_404
+from django.conf import settings
+from django.http import JsonResponse
+from django.views.decorators.cache import cache_page
 
 from .forms import BalanceForm
 
 User = get_user_model()
 
 
+@cache_page(60 * 5)
+def settings_api(request):
+    return JsonResponse(settings.SITE_OPTIONS)
+
+
 def pay(request, pk):
+    """View-функция, отнимающая баланс по данным из POST запроса."""
     user = get_object_or_404(User, pk=pk)
     if request.method == 'POST':
         form = BalanceForm(request.POST)
